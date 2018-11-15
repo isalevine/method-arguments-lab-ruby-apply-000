26217f6021f69b8ed51a74c1c5ddb765311d23d5
Isa Levine <isaacdlevine@gmail.com>
2018-11-15 19:38:32 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,4 @@ def introduction_with_language(name, code)
   puts "Hi, my name is #{name} and I'm learning to code in #{code}--pleasure to meet you!"
 end
 
-introduction_with_language("Isa", "Ruby")
+
